694aac28fbfab973dfbcc978ef086d696c615e67
seulbin gu <tmfqls12311@gmail.com>
2024-01-09 23:40:13 +0900

    인덴트 수정

--- src/com/sugang/dto/DtoSugang.java
+++ src/com/sugang/dto/DtoSugang.java
@@ -1,40 +1,46 @@
 package com.sugang.dto;
 
 public class DtoSugang {
-	private String st_no;
-	private String class_no;
-	private int class_credit;
-	
-	public DtoSugang() {}
+    private String st_no;
+    private String class_no;
+    private int class_credit;
 
-	public DtoSugang(String st_no, String class_no, int class_credit) {
-		this.st_no = st_no;
-		this.class_no = class_no;
-		this.class_credit = class_credit;
-	}
+    public DtoSugang() {
+    }
 
-	@Override
-	public String toString() {
-		return "DtoSugang [st_no=" + st_no + ", class_no=" + class_no + ", class_credit=" + class_credit + "]";
-	}
+    public DtoSugang(String st_no, String class_no, int class_credit) {
+        this.st_no = st_no;
+        this.class_no = class_no;
+        this.class_credit = class_credit;
+    }
 
-	public int getClass_credit() {
-		return class_credit;
-	}
-	public void setClass_credit(int class_credit) {
-		this.class_credit = class_credit;
-	}
-	public String getClass_no() {
-		return class_no;
-	}
-	public void setClass_no(String class_no) {
-		this.class_no = class_no;
-	}
-	public String getSt_no() {
-		return st_no;
-	}
-	public void setSt_no(String st_no) {
-		this.st_no = st_no;
-	}
+    @Override
+    public String toString() {
+        return "DtoSugang [st_no=" + st_no + ", class_no=" + class_no + ", class_credit=" + class_credit + "]";
+    }
+
+    public int getClass_credit() {
+        return class_credit;
+    }
+
+    public void setClass_credit(int class_credit) {
+        this.class_credit = class_credit;
+    }
+
+    public String getClass_no() {
+        return class_no;
+    }
+
+    public void setClass_no(String class_no) {
+        this.class_no = class_no;
+    }
+
+    public String getSt_no() {
+        return st_no;
+    }
+
+    public void setSt_no(String st_no) {
+        this.st_no = st_no;
+    }
 
 }
--- src/com/sugang/util/DBConn.java
+++ src/com/sugang/util/DBConn.java
@@ -7,77 +7,83 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBConn {
-	private DBConn() {}
-	public static Connection dbConn = null;
-	private static Statement st = null;
-	private static ResultSet rs = null;
+    private DBConn() {
+    }
 
-	public static Connection getInstance() throws Exception {
-		if(dbConn == null) {
-			try {
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-				String url = "jdbc:oracle:thin:@localhost:1521:xe";
-				System.out.println("Enter DB Id >>");
-				String user = new java.util.Scanner(System.in).nextLine();
-				System.out.println("Enter db Password >>");
-				String pw = new java.util.Scanner(System.in).nextLine();
-				dbConn = DriverManager.getConnection(url, user, pw);
-				System.out.println("DrivermanagerConnection...");
-			} catch (ClassNotFoundException e) {
-				throw new Exception("데이터베이스 연결 실패 !!");
-			} catch (SQLException e) {
-				throw new Exception("사용자 조회 오류 !!");
-			}
-		}
-		return dbConn;
-	}
-	public static int statementUpdate(String sql) throws Exception {
-		int resultValue = 0;
-			try {
-				if(st== null) {
-					st = dbConn.createStatement();
-				}
-				resultValue = st.executeUpdate(sql);
-				System.out.println("StatementExcuteUpdate...");
-			} catch (SQLException e) {
-				throw new Exception("수정 실패 !!");
-			}
-		return resultValue;
-	}
-	public static ResultSet statementQuery(String sql) throws Exception {
-			try {
-				if(st == null) {
-				st = dbConn.createStatement();
-				System.out.println("statementQuery...");
-				}
-				rs = st.executeQuery(sql);
-			} catch (SQLException e) {
-				throw new Exception("조회 실패 !!");
-			}
-		return rs;
-	}
-	public static void dbClose() throws Exception {
-			try {
-				if (rs!= null)rs.close();
-				if (st!= null)st.close();
-				if (dbConn!= null)dbConn.close();
-			} catch (SQLException e) {
-				throw new Exception("데이터베이스 종료 실패 !!");
-			}finally {
-				rs = null;
-				st = null;
-				dbConn = null;
-			}
-	}
-	public static void dbReset() throws Exception {
-		try {
-			if (rs!= null)rs.close();
-			if (st!= null)st.close();
-		} catch (SQLException e) {
-			throw new Exception("데이터베이스 초기화 실패 !!");
-		}finally {
-			rs = null;
-			st = null;
-		}
-	}
+    public static Connection dbConn = null;
+    private static Statement st = null;
+    private static ResultSet rs = null;
+
+    public static Connection getInstance() throws Exception {
+        if (dbConn == null) {
+            try {
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                String url = "jdbc:oracle:thin:@localhost:1521:xe";
+                System.out.println("Enter DB Id >>");
+                String user = new java.util.Scanner(System.in).nextLine();
+                System.out.println("Enter db Password >>");
+                String pw = new java.util.Scanner(System.in).nextLine();
+                dbConn = DriverManager.getConnection(url, user, pw);
+                System.out.println("DrivermanagerConnection...");
+            } catch (ClassNotFoundException e) {
+                throw new Exception("데이터베이스 연결 실패 !!");
+            } catch (SQLException e) {
+                throw new Exception("사용자 조회 오류 !!");
+            }
+        }
+        return dbConn;
+    }
+
+    public static int statementUpdate(String sql) throws Exception {
+        int resultValue = 0;
+        try {
+            if (st == null) {
+                st = dbConn.createStatement();
+            }
+            resultValue = st.executeUpdate(sql);
+            System.out.println("StatementExcuteUpdate...");
+        } catch (SQLException e) {
+            throw new Exception("수정 실패 !!");
+        }
+        return resultValue;
+    }
+
+    public static ResultSet statementQuery(String sql) throws Exception {
+        try {
+            if (st == null) {
+                st = dbConn.createStatement();
+                System.out.println("statementQuery...");
+            }
+            rs = st.executeQuery(sql);
+        } catch (SQLException e) {
+            throw new Exception("조회 실패 !!");
+        }
+        return rs;
+    }
+
+    public static void dbClose() throws Exception {
+        try {
+            if (rs != null) rs.close();
+            if (st != null) st.close();
+            if (dbConn != null) dbConn.close();
+        } catch (SQLException e) {
+            throw new Exception("데이터베이스 종료 실패 !!");
+        } finally {
+            rs = null;
+            st = null;
+            dbConn = null;
+        }
+    }
+
+    public static void dbReset() throws Exception {
+        try {
+            if (rs != null) rs.close();
+            if (st != null) st.close();
+        } catch (SQLException e) {
+            throw new Exception("데이터베이스 초기화 실패 !!");
+        } finally {
+            rs = null;
+            st = null;
+        }
+    }
 }
